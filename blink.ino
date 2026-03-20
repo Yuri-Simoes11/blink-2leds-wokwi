6a0905a15de5efe932d83dd7756380987a25ec41
Yuri-Simoes11 <yuri.orikassa@usp.br>
2026-03-20 11:01:00 -0300

    Update blink.ino
    Inserção do código do semáforo com comentários

--- blink.ino
+++ blink.ino
@@ -1,40 +1,27 @@
-/*
-  Blink
+int LEDVermelho = 13;
+int LEDAmarelo = 12;
+int LEDVerde = 11;
+int TempoVerde = 1000; //Modifique essa variável se quiser alterar o tempo do semáforo
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
+  // put your setup code here, to run once:
+    pinMode(LEDVermelho, OUTPUT);
+    pinMode(LEDAmarelo, OUTPUT);
+    pinMode(LEDVerde, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(8, LOW);
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(8, HIGH);
-  delay(1000);                       // wait for a second
+  // put your main code here, to run repeatedly:
+    digitalWrite(LEDVerde, HIGH);
+    digitalWrite(LEDAmarelo, LOW);
+    digitalWrite(LEDVermelho, LOW);
+    delay(TempoVerde);
+    digitalWrite(LEDVerde, LOW);
+    digitalWrite(LEDAmarelo, HIGH);
+    digitalWrite(LEDVermelho, LOW);
+    delay(TempoVerde * 2); //Se quiser mudar a duração, mude o multiplicador
+    digitalWrite(LEDVerde, LOW);
+    digitalWrite(LEDAmarelo, LOW);
+    digitalWrite(LEDVermelho, HIGH);
+    delay(TempoVerde * 5); //Mesma informação, edite o multiplicador se quiser
 }
